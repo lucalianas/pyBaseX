28b7cadf3553c3cb22bda2f363d5d2eeffedc4d5
Luca Lianas <lucalianas@gmail.com>
2014-12-17 16:08:51 +0100

    added a way to have a 'silent' logger (useful to developers)

--- pybasex/utils/__init__.py
+++ pybasex/utils/__init__.py
@@ -4,19 +4,26 @@ LOG_FORMAT = '%(asctime)s|%(levelname)-8s|%(message)s'
 LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'
 
 
-def get_logger(name, log_level='INFO', log_file=None, mode='a'):
+def get_logger(name, level='INFO', file=None, mode='a', silent=False):
+    # clean root logger
+    _ = logging.getLogger()
+    _.handlers = []
+    # set logger now
     logger = logging.getLogger(name)
     try:
-        log_level = getattr(logging, log_level)
+        level = getattr(logging, level)
     except AttributeError:
-        raise ValueError('unsupported literal log level: %s' % log_level)
-    logger.setLevel(log_level)
+        raise ValueError('unsupported literal log level: %s' % level)
+    logger.setLevel(level)
     # clear existing handlers
     logger.handlers = []
-    if log_file:
-        handler = logging.FileHandler(log_file, mode=mode)
+    if silent:
+        handler = logging.NullHandler()
     else:
-        handler = logging.StreamHandler()
+        if file:
+            handler = logging.FileHandler(file, mode=mode)
+        else:
+            handler = logging.StreamHandler()
     formatter = logging.Formatter(LOG_FORMAT, datefmt=LOG_DATEFMT)
     handler.setFormatter(formatter)
     logger.addHandler(handler)
