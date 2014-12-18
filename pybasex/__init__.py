d76cd4c4d6cb7d41f75fc8be798018b7c62a7415
Luca Lianas <lucalianas@gmail.com>
2014-12-18 21:30:38 +0100

    fixed RST text

--- pybasex/__init__.py
+++ pybasex/__init__.py
@@ -2,8 +2,8 @@
 pyBaseX: a Python adapter for BaseX REST interface
 --------------------------------------------------
 
-pyBaseX is a Python package that provides functionality to interact
-with BaseX via REST interface.
+pyBaseX is a Python package that provides functionality to interact with BaseX via REST interface.
+
 The main features include:
  * CRUD methods for databases and documents
  * XPATH queries execution
