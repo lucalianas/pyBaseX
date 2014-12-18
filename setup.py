aa19840a4bdbf40e18840ea59ff4fe9320d52258
Luca Lianas <lucalianas@gmail.com>
2014-12-18 17:32:33 +0100

    extended setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 from distutils.errors import DistutilsSetupError
 
+import pybasex
+
 AUTHOR_INFO = [
     ('Luca Lianas', 'lucalianas@gmail.com')
 ]
@@ -9,6 +11,7 @@ AUTHOR = '', ''.join(t[0] for t in AUTHOR_INFO)
 AUTHOR_EMAIL = '', ''.join('<%s>' % t[1] for t in AUTHOR_INFO)
 MAINTAINER = ", ".join(t[0] for t in MAINTAINER_INFO)
 MAINTAINER_EMAIL = ", ".join("<%s>" % t[1] for t in MAINTAINER_INFO)
+URL = 'https://github.com/lucalianas/pyBaseX'
 
 try:
     with open("NAME") as f:
@@ -21,10 +24,20 @@ except IOError:
 setup(
     name=NAME,
     version=VERSION,
+    description=pybasex.__doc__.strip().splitlines()[0],
+    long_description=pybasex.__doc__.strip(),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
+    url=URL,
+    license='MIT License',
+    keyword=['BaseX', 'REST', 'HTTP', 'XPATH', 'XML', 'database', 'python'],
     maintainer=MAINTAINER,
     maintainer_email=MAINTAINER_EMAIL,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
     packages=[
         'pybasex',
         'pybasex.utils',
